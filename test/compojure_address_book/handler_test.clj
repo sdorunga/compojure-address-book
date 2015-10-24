a535f595575571b30e9e56e8191f4875d116ebd5
Stefan Dorunga <stefandorunga@gmail.com>
2015-10-24 22:44:34 +0100

    Add contacts and new contact form

--- test/compojure_address_book/handler_test.clj
+++ test/compojure_address_book/handler_test.clj
@@ -1,17 +1,22 @@
 (ns compojure-address-book.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [compojure-address-book.handler :refer :all]))
+            [compojure-address-book.handler :refer :all]
+            [compojure-address-book.core.routes.address-book-routes :refer [contacts]]))
 
 (deftest test-app
   (testing "Test GET"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (.contains (:body response) "Example GET"))))
+      (is (.contains (:body response) "Jarrod Taylor"))
+      (is (.contains (:body response) "Johnny Utah"))
+      (is (.contains (:body response) "James Dalton"))))
+
   (testing "Test POST"
-    (let [response (app (mock/request :post "/post" {:example-post "Some data"}))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "You posted: Some data"))))
+    (let [response (app (mock/request :post "/post" {:name "Joseph" :phone "1234" :email "josephstalin@cccp.com"}))
+          new-contact (filter #(= 4 (:id %)) @contacts)]
+      (is (= (:status response) 302))
+      (is (= (:name (first new-contact)) "Joseph"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
